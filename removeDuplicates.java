57c37ad883a9883c301359e401c63df04de3ef5e
Prithvi Dev <prithvidevkanojia@gmail.com>
2020-12-16 21:10:50 +0530

    Upload

---
+++ removeDuplicates.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != nums[count]){
+                count++;
+                nums[count]=nums[i];
+            }
+        }
+        return count+1;
+    }
+}
