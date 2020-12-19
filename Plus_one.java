608c0bd8ce7214c3ff626b0d25c5a1346dd4428c
Prithvi Dev <prithvidevkanojia@gmail.com>
2020-12-19 21:10:00 +0530

    Upload

---
+++ Plus_one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i= digits.length-1; i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newN = new int[digits.length +1];
+        newN[0]=1;
+        return newN;
+    }
+}
